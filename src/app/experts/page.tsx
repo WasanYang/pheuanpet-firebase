2bdafb2e12a079b5f61f4092bc8f73f50524e1e4
Es Yaj <esxy26@gmail.com>
2025-06-30 15:20:49 +0000

    อยากแสดง การ์ดขั้นต่ำ 2 การ์ดดีกว่า

--- src/app/experts/page.tsx
+++ src/app/experts/page.tsx
@@ -69,7 +69,7 @@ export default function VetConnectDirectoryPage() {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0.5">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-0.5">
             {filteredExperts.map(expert => (
                 <ExpertCard key={expert.id} expert={expert} />
             ))}
